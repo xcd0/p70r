03579d9af1178c008cb64633f55de062ef47153a
xcd0 <xcd0@xcd0.com>
2023-12-15 01:40:27 +0900

    tmp

--- keyboards/p70r/keymaps/default/keymap.c
+++ keyboards/p70r/keymaps/default/keymap.c
@@ -7,26 +7,86 @@
 #include "keymap_japanese.h"
 #include "p70r.h"
 
+// #define JP_SCLN KC_SCLN // ;
+// #define JP_COLN KC_QUOT // :
+// #define JP_AT   KC_LBRC // @
+// #define JP_LBRC KC_RBRC // [
+// #define JP_RBRC KC_NUHS // ]
+// #define JP_LPRN S(JP_8)    // (
+// #define JP_RPRN S(JP_9)    // )
+// #define JP_LCBR S(JP_LBRC) // {
+// #define JP_RCBR S(JP_RBRC) // }
+// #define JP_EQL  S(JP_MINS) // =
+// #define JP_TILD S(JP_CIRC) // ~
+// #define JP_PIPE S(JP_YEN)  // |
+// #define JP_GRV  S(JP_AT)   // `
+// #define JP_CAPS S(JP_EISU) // Caps Lock
+// #define JP_EXLM S(JP_1)    // !
+// #define JP_DQUO S(JP_2)    // "
+// #define JP_HASH S(JP_3)    // #
+// #define JP_DLR  S(JP_4)    // $
+// #define JP_PERC S(JP_5)    // %
+// #define JP_AMPR S(JP_6)    // &
+// #define JP_QUOT S(JP_7)    // '
+// #define JP_LPRN S(JP_8)    // (
+// #define JP_RPRN S(JP_9)    // )
+// #define JP_EQL  S(JP_MINS) // =
+// #define JP_TILD S(JP_CIRC) // ~
+// #define JP_PIPE S(JP_YEN)  // |
+// #define JP_GRV  S(JP_AT)   // `
+// #define JP_LCBR S(JP_LBRC) // {
+// #define JP_CAPS S(JP_EISU) // Caps Lock
+// #define JP_PLUS S(JP_SCLN) // +
+// #define JP_ASTR S(JP_COLN) // *
+// #define JP_RCBR S(JP_RBRC) // }
+// #define JP_LABK S(JP_COMM) // <
+// #define JP_RABK S(JP_DOT)  // >
+// #define JP_QUES S(JP_SLSH) // ?
+// #define JP_UNDS S(JP_BSLS) // _
+
+#define L3_ESC LT(3,KC_ESC)
+#define SFT_SPC MT(MOD_LSFT,KC_SPC)
+
 ///*
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-    QK_BOOT, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,   LSFT(KC_MINS),                   LSFT(KC_8), KC_6, KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS,
-    KC_ESC,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,   KC_UNDS,                         KC_ASTR, KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,    KC_GRV,
-    KC_TAB,  KC_A,    KC_S,   KC_D,    KC_F,    KC_G,   KC_MINS,     MO(1),      MO(1),  KC_LBRC, KC_H,    KC_J,  KC_K,    KC_L,   KC_EQL,  KC_SCLN,
-    KC_LALT, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,   LSFT(KC_NO), MO(1),      MO(1),  KC_LCBR, KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_INT1,
-                              KC_INT5, MO(2),  MO(1), LSFT_T(KC_SPC), KC_LCTL,   KC_DEL, KC_BSPC, KC_ENT,  MO(3), KC_INT4,
-                                                 KC_P3,                                                         KC_P7,
-                                          KC_P1, KC_P4, KC_P2,                                           KC_P5, KC_P8, KC_P6
-    ),
-    [1] = LAYOUT(
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, KC_QUOT, KC_NO, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_NO, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, LSFT(KC_QUOT), KC_TRNS, KC_TRNS, KC_NO, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, LSFT(KC_0), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCTL, KC_INS, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_C, KC_G, KC_A, KC_D, KC_B, KC_E, KC_H, KC_F
-    ),
-    [2] = LAYOUT(
-    QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_NO, KC_NO, KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
-    ),
-    [3] = LAYOUT(
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LGUI(KC_V), LGUI(KC_DOT), KC_NO, KC_NO, SGUI(KC_S), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO, KC_NO, KC_NO, KC_NO, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
-    )
+
+    [0] = LAYOUT (
+            L3_ESC,  JP_1,    JP_2,    JP_3,    JP_4,    JP_5,    S(JP_MINS),                     JP_LPRN, JP_6,    JP_7,    JP_8,    JP_9,    JP_0,    JP_MINS,
+            L3_ESC,  JP_Q,    JP_W,    JP_E,    JP_R,    JP_T,    S(JP_MINS),                     JP_LPRN, JP_Y,    JP_U,    JP_I,    JP_O,    JP_P,    JP_AT,
+            KC_TAB,  JP_A,    JP_S,    JP_D,    JP_F,    JP_G,    JP_MINS,   MO(1),       MO(1),  JP_LBRC, JP_H,    JP_J,    JP_K,    JP_L,    JP_SCLN, JP_COLN,
+            KC_LALT, JP_Z,    JP_X,    JP_C,    JP_V,    JP_B,    JP_UNDS,   MO(1),       MO(1),  JP_LCBR, JP_N,    JP_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_UNDS,
+                                       JP_MHEN, MO(2),   MO(1),   SFT_SPC,   KC_LCTL,     KC_DEL, KC_BSPC, KC_ENT,  MO(3),   LT(2,JP_HENK),
+                                                                  KC_P1,                          KC_P5,
+                                                         KC_P2,   KC_P3,   KC_P4,         KC_P6,  KC_P7,   KC_P8
+            ),
+    [1] = LAYOUT (
+            KC_NO, KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,   KC_NO,                             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+            KC_NO, KC_1,    KC_2,    KC_3,      KC_4,      KC_5,    KC_NO,                             JP_RPRN, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
+            KC_NO, JP_EXLM, JP_DQUO, JP_HASH,   JP_DLR,    JP_PERC, JP_ASTR, KC_TRNS,         KC_TRNS, JP_RBRC, JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_EQL,  JP_TILD,
+            KC_NO, KC_NO,   KC_NO,   C(KC_INS), S(KC_INS), KC_NO,   JP_PIPE, KC_TRNS,         KC_TRNS, JP_RCBR, KC_NO,   KC_NO,   KC_COMM, KC_DOT,  KC_SLSH, JP_UNDS,
+                                     KC_NO,     KC_NO,     KC_TRNS, KC_NO,   KC_NO,           KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,
+                                                                    KC_A,                                 KC_E,
+                                                           KC_B,    KC_C,       KC_D,            KC_F,    KC_G,       KC_H
+            ),
+    [2] = LAYOUT (
+            QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,                  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,
+            KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,                  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,
+            KC_NO,   KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5,  KC_F6,  KC_NO,   KC_NO, KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END, KC_NO,
+            KC_NO,   KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,
+                                   KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                                          KC_NO,                  KC_NO,
+                                                   KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO
+            ),
+    [3] = LAYOUT (
+            KC_TRNS, G(KC_V), G(KC_DOT), KC_NO, KC_PSCR, LSG(KC_S), KC_NO,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_TRNS, G(KC_V), G(KC_DOT), KC_NO, KC_PSCR, LSG(KC_S), KC_NO,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO,   KC_1,    KC_2,      KC_3,  KC_4,    KC_5,      KC_NO, KC_NO,       KC_NO, KC_NO, KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_NO,
+            QK_BOOT, KC_NO,   KC_NO,     KC_NO, KC_NO,   KC_NO,     KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                                                    KC_NO,                     KC_NO,
+                                                             KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO
+            ),
+    [4] = LAYOUT( KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
 };
 //*/
 
